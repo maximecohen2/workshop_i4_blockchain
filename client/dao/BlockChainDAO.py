fe9127b16f9fe22874251ed90ab3f1b7bf5861c8
EnzoTUI <etuihagi@gmail.com>
2018-11-07 10:20:02 +0100

    [I4-WorkShop]dev API Catastrophe

--- client/dao/BlockChainDAO.py
+++ client/dao/BlockChainDAO.py
@@ -1,6 +1,7 @@
 import json
 import requests
 
+from urllib.parse import urlencode
 from client.dao.model import CatastropheData
 from client.dao.model.Manager import Manager
 
@@ -8,10 +9,25 @@ from client.dao.model.Manager import Manager
 class BlockChainDAO:
 
     def __init__(self):
-        self.baseUrl = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=gaspar-arretes-de-catastrophes-naturelles&q=Talence&rows=10&sort=dat_pub_arrete"
+        self.baseUrl = "https://public.opendatasoft.com/api/records/1.0/search/"
         self.manager = Manager()
-    def responseapi(self):
-        response = requests.get(self.baseUrl)
+
+    def getDataCatastrophe(self, commmune):
+        urlParams = self.buildParamsUrl(commmune)
+        return self.responseapi(urlParams)
+
+    def buildParamsUrl(self, commune):
+        params = {}
+        params['dataset'] = "gaspar-arretes-de-catastrophes-naturelles"
+        params['q'] = commune
+        params['rows'] = 15
+        params['sort'] = "dat_pub_arrete"
+        return params
+
+    def responseapi(self, paramsUrl):
+        urlEncode = urlencode(paramsUrl)
+        url = self.baseUrl + '?' + urlEncode
+        response = requests.get(url)
         data = json.loads(response.content)
         cataList = [CatastropheData]
         for k in data['records']:
@@ -22,4 +38,4 @@ class BlockChainDAO:
 
 
 blockChainDAO = BlockChainDAO()
-blockChainDAO.responseapi()
+blockChainDAO.getDataCatastrophe("Talence")
