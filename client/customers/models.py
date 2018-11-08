c0500e43200a01e56d331c29bf0ddda232fe021d
Peter Xu <peter@localhost.peterxu>
2018-11-08 17:46:32 +0100

    Add data to SGDB
    Add data from form to database

--- client/customers/models.py
+++ client/customers/models.py
@@ -4,22 +4,18 @@ from django.core.validators import MinValueValidator
 # Create your models here.
 
 class Address(models.Model):
-    houseNumber = models.IntegerField(validators=[MinValueValidator(1)])
-    houseStreet = models.CharField(max_length=255)
+    houseNumber = models.IntegerField(validators=[MinValueValidator(1)], null=True)
+    houseStreet = models.CharField(max_length=255, null=True)
     postalCode = models.CharField(max_length=10)
     city = models.CharField(max_length=255)
-    country = models.CharField(max_length=255)
+    country = models.CharField(max_length=255, null=True)
 
 class Customer(models.Model):
+    civilite = models.CharField(max_length=5)
     lastName = models.CharField(max_length=128)
     firstName = models.CharField(max_length=128)
-    birthDate = models.DateField()
-    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    birthDate = models.DateField(null=True)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, null=True)
 
 class Contract(models.Model):
-    customer = models.OneToOneField(
-        Customer,
-        on_delete=models.CASCADE,
-        primary_key=True,
-    )
     contractNumber = models.CharField(max_length=30)
--- client/customers/views.py
+++ client/customers/views.py
@@ -2,13 +2,31 @@ from django.shortcuts import render
 from customers.models import *
 from .form import InscriptionForm
 
+
 def inscription(request):
-    form = InscriptionForm(request.POST or None)
-    if form.is_valid():
-        civilite = form.cleaned_data['civilite']
+    form = InscriptionForm(None)
     return render(request, 'inscription.html', locals())
 
 def success(request):
+    form = InscriptionForm(request.POST)
+    if form.is_valid():
+        customer = Customer()
+        address = Address()
+        contract = Contract()
+
+        customer.civilite = form.cleaned_data['civilite']
+        customer.lastName = form.cleaned_data['lastName']
+        customer.firstName = form.cleaned_data['firstName']
+
+        address.postalCode = form.cleaned_data['postalCode']
+        address.city = form.cleaned_data['city']
+
+        contract.contractNumber = form.cleaned_data['contractNumber']
+        
+        customer.save()
+        address.save()
+        contract.save()
+        
     return render(request, 'success.html', locals())
 
 def getPersonByCommune(commune):
